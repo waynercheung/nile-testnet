6a3c3772e48145e6b448c0033798a0b64bf5e2e5
waynercheung <waynercheung@gmail.com>
2025-05-27 11:59:03 +0800

    feat: use BufferedReader instead of Stream API (lines().collect()), try to catch IOException instead of UncheckedIOException

--- framework/src/main/java/org/tron/core/services/http/PostParams.java
+++ framework/src/main/java/org/tron/core/services/http/PostParams.java
@@ -3,7 +3,7 @@ package org.tron.core.services.http;
 import static org.apache.http.entity.ContentType.APPLICATION_FORM_URLENCODED;
 import static org.tron.core.services.http.Util.getJsonString;
 
-import java.util.stream.Collectors;
+import java.io.BufferedReader;
 import javax.servlet.http.HttpServletRequest;
 import lombok.Getter;
 
@@ -22,7 +22,18 @@ public class PostParams {
   }
 
   public static PostParams getPostParams(HttpServletRequest request) throws Exception {
-    String input = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+    // String input = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+    StringBuilder builder = new StringBuilder();
+    try (BufferedReader reader = request.getReader()) {
+      char[] buffer = new char[8192];
+      int charsRead;
+      while ((charsRead = reader.read(buffer)) != -1) {
+        builder.append(buffer, 0, charsRead);
+      }
+    }
+    String input = builder.toString();
+
     Util.checkBodySize(input);
     if (APPLICATION_FORM_URLENCODED.getMimeType().equals(request.getContentType())) {
       input = getJsonString(input);
